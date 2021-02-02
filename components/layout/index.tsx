1b0e2f6055f1b7fa0a8c219800a12b833c4fbc16
Bayu Aditya <bayu.aditya@sci.ui.ac.id>
2021-02-02 22:31:01 +0700

    checkpoint

--- components/layout/index.tsx
+++ components/layout/index.tsx
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { 
   AppBar, 
+  Button, 
+  Menu, 
+  MenuItem, 
   Toolbar, 
   Typography } from "@material-ui/core";
 
 import styles from "./index.module.scss";
 
 export const MainLayout: React.FC = (props) => {
+  const [openMode, setOpenMode] = useState<boolean>(false)
+  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
+
+  const handleOpenMode = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setAnchorEl(e.currentTarget)
+    setOpenMode(true)
+  }
+  const handleCloseMode = () => {
+    setAnchorEl(null)
+    setOpenMode(false)
+  }
+
+  const handleChangeMode = (value: "simulation" | "experiment") => {
+    console.log(value)
+
+    if (value === "simulation") {
+      window.location.href = "/simulation"
+    } else if (value === "experiment") {
+      window.location.href = "/experiment"
+    }
+  }
+
   return (
     <div className={styles.root}>
       <AppBar className={styles.appbar}>
@@ -15,6 +40,28 @@ export const MainLayout: React.FC = (props) => {
             variant="h6"
             className={styles.title}
           >Reflection Loss</Typography>
+
+          <Button
+            aria-controls="mode-popout"
+            aria-haspopup
+            onClick={handleOpenMode}
+            className={styles.button}
+          >Mode</Button>
+
+          <Menu 
+            id="mode-popout"
+            open={openMode} 
+            keepMounted
+            anchorEl={anchorEl}
+            onClose={handleCloseMode}
+          >            
+            <MenuItem
+              onChange={() => handleChangeMode("simulation")}
+            >Simulation</MenuItem>
+            <MenuItem 
+              onChange={() => handleChangeMode("experiment")} 
+            >Experiment</MenuItem>
+          </Menu>
         </Toolbar>
       </AppBar>
 
